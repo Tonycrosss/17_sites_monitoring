9117290716a2049c21f81fafce9c226d58e48a7f
TonyCross <starovoitov.anton@gmail.com>
2017-11-08 12:43:27 +0000

    Exp date get added

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,4 +1,7 @@
 import requests
+import json
+import whois
+from urllib.parse import urlparse
 
 
 def load_urls4check():
@@ -9,16 +12,28 @@ def load_urls4check():
 
 def is_server_respond_with_200(url):
     response = requests.get(url)
-    print(response)
+    print(response.status_code)
 
+
+def get_domain_name(url):
+    parsed_uri = urlparse(url)
+    domain_name = parsed_uri.netloc
+    return domain_name
+    
+    
+
+# TODO: change to params
 def get_domain_expiration_date(domain_name):
-    pass
+    domain_info = whois.whois('{}'.format(domain_name))
+    print(domain_info.expiration_date)
 
 
 def main():
     url_list = load_urls4check()
     for url in url_list:
-        is_server_respond_with_200(url)
+        status_code = is_server_respond_with_200(url)
+        domain_name = get_domain_name(url)
+        exp_date = get_domain_expiration_date(domain_name)
 
 
 if __name__ == '__main__':
