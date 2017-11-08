88c456369beab86e72752856c684fbce871fbac2
Старовойтов Антон Станиславович <AStarovoytov@alfabank.ru>
2017-11-08 13:21:40 +0500

    загрузка файла + проверка ссылки

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,25 @@
-def load_urls4check(path):
-    pass
+import requests
+
+
+def load_urls4check():
+    with open('./links.txt') as file_handler:
+        urls_list = file_handler.readlines()
+    return urls_list
+
 
 def is_server_respond_with_200(url):
-    pass
+    response = requests.get(url)
+    print(response)
 
 def get_domain_expiration_date(domain_name):
     pass
 
+
+def main():
+    url_list = load_urls4check()
+    for url in url_list:
+        is_server_respond_with_200(url)
+
+
 if __name__ == '__main__':
-    pass
+    main()
